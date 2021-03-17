dec09c06deca163ea9b7639caad3d0cefeb558f2
Marcelo Morais <m2marcelo@gmail.com>
2021-03-17 22:50:14 +0100

    Updating the MainViewModel to use the database

--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,27 @@
 package com.udacity.asteroidradar.main
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.database.AsteroidDatabaseDao
 
-class MainViewModel : ViewModel() {
+class MainViewModel(val database: AsteroidDatabaseDao, application: Application) : AndroidViewModel(application) {
+
+    val asteroids = database.getAll()
+
+    private val _asteroidDetail = MutableLiveData<Asteroid?>()
+
+    val asteroidDetail : LiveData<Asteroid?>
+        get() = _asteroidDetail
+
+    fun onAsteroidClicked(asteroid: Asteroid) {
+        _asteroidDetail.value = asteroid
+    }
+
+    fun onAsteroidNavigated(){
+        _asteroidDetail.value = null
+    }
 }
