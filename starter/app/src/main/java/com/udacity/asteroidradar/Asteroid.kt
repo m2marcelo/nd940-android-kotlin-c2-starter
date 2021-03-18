f5541e2c3334925dd42cef3b79f71b0b32925a6b
Marcelo Morais <m2marcelo@gmail.com>
2021-03-18 15:14:43 +0100

    Making asteroid as entity

--- starter/app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
@@ -2,9 +2,23 @@ package com.udacity.asteroidradar
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
+import com.udacity.asteroidradar.database.Asteroid as Entity
 
 @Parcelize
 data class Asteroid(val id: Long, val codename: String, val closeApproachDate: String,
                     val absoluteMagnitude: Double, val estimatedDiameter: Double,
                     val relativeVelocity: Double, val distanceFromEarth: Double,
                     val isPotentiallyHazardous: Boolean) : Parcelable
+
+fun Asteroid.toEntity() {
+    Entity(
+        id = this.id,
+        codename = this.codename,
+        closeApproachDate = this.closeApproachDate,
+        absoluteMagnitude = this.absoluteMagnitude,
+        estimatedDiameter = this.estimatedDiameter,
+        relativeVelocity = this.relativeVelocity,
+        distanceFromEarth = this.distanceFromEarth,
+        isPotentiallyHazardous = this.isPotentiallyHazardous
+    )
+}
